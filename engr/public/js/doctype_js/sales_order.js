1df73ac981b865c00346aa85593c40f212bc571e
Ubuntu <ubuntu@ip-172-31-24-171.ap-south-1.compute.internal>
2021-04-05 07:25:37 +0000

    Fiscal and company_series field added in proforma invoice

--- engr/public/js/doctype_js/sales_order.js
+++ engr/public/js/doctype_js/sales_order.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Sales Order', {
     refresh: function(frm) {
-            frm.add_custom_button(__('Create Proforma Invoice'),function() {frm.trigger('create_proforma_invoice')}, __('Create'));
+            frm.add_custom_button(__('Proforma Invoice'),function() {frm.trigger('create_proforma_invoice')}, __('Create'));
 	},
     create_proforma_invoice: function(frm){
         frappe.model.open_mapped_doc({
