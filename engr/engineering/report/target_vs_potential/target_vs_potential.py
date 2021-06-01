bc13180a78baba0c7e0e97c14879a38884e57649
finbyz <info@finbyz.com>
2021-06-01 07:25:19 +0000

    Target vs Potential CHanges for exporting into excel

--- engr/engineering/report/target_vs_potential/target_vs_potential.py
+++ engr/engineering/report/target_vs_potential/target_vs_potential.py
@@ -33,42 +33,44 @@ def get_data(filters):
 		item_group_list.append(row.item_group)
 		total_potential = 0
 		if row.customer not in customer_ig:
-
 			# customer_ig[row.customer] = [{row.item_group:'{} | {}'.format(str(int(row.potential)),str(int(row.target)))}]
-			customer_ig[row.customer] = [{row.item_group:'<table width="100%"><tr><td width="49%"><p style="margin: 0;padding: 0;">{}</p></td><td width="2%"><p style="margin: 0;padding: 0;">|</p></td><td width="49%"><p style="margin: 0;text-align:right !important;padding: 0;">{}</p></td></tr></table>'.format(str(int(row.potential or 0)),str(int(row.target or 0)))}]
+			customer_ig[row.customer] = [{row.item_group:'<div style="display: flex;"><p style="margin: 0;text-align:left !important;padding: 0; width: 90px;">{}</p>|<p style="margin: 0;text-align:right !important;padding: 0; width: 90px;">{}</p></div>'.format(str(int(row.potential or 0)),str(int(row.target or 0)))}]
 			customer_ig[row.customer].append({'sales_person':row.sales_person})
 		else:
-			customer_ig[row.customer].append({row.item_group:'<table width="100%"><tr><td width="49%"><p style="margin: 0;padding: 0;">{}</p></td><td width="2%"><p style="margin: 0;padding: 0;">|</p></td><td width="49%"><p style="margin: 0;text-align:right !important;padding: 0;">{}</p></td></tr></table>'.format(str(int(row.potential or 0)),str(int(row.target or 0)))})
+			# customer_ig[row.customer].append({row.item_group:'<table width="100%"  style="border: 0px;"><tr><td width="49%"><p style="margin: 0;padding: 0;">{}</p></td><td width="2%"><p style="margin: 0;padding: 0;">|</p></td><td width="49%"><p style="margin: 0;text-align:right !important;padding: 0;">{}</p></td></tr></table>'.format(str(int(row.potential or 0)),str(int(row.target or 0)))})
+			customer_ig[row.customer].append({row.item_group:'<div style="display: flex;"><p style="margin: 0;text-align:left !important;padding: 0; width: 90px;">{}</p>|<p style="margin: 0;text-align:right !important;padding: 0; width: 90px;">{}</p></div>'.format(str(int(row.potential or 0)),str(int(row.target or 0)))})
+
 			# <p><spam style="text-align:right !important; margin: 0;padding: 0;">{}| <spam style="text-align:right !important;text-align:right;margin: 0;padding: 0;">{}</spam></spam></p>
 	data=customize_data(customer_ig)
 	return data,list(set(item_group_list))
 
 def customize_data(customer_ig):
 	lst=[]
-	lst.append({"customer":'',"sales_person":''})
-	for key,value in customer_ig.items():
-		total_potential,total_target = 0,0
-		each_customer={}
-		each_customer["customer"]= key
-		# each_customer['sales_person']=
-		for each in value:
-			for ka,va in each.items():
-				if ka !='sales_person':
-					lst[0].update({ka:'<table width="100%"><tr><td width="49%"><p style="margin: 0;padding: 0;">Potential </p></td><td width="2%"><p style="margin: 0;padding: 0;">|</p></td><td width="49%"><p style="margin: 0;padding: 0;text-align:right !important;"> Target</p></td></tr></table>'})
-					each_customer[ka]=va
-					if (va):
-						try:
-							total_potential_value= (va.split('padding: 0;">'))
-							total_potential += flt(total_potential_value[1].split('</p></td><td width=')[0])
-							total_target += flt(total_potential_value[3].split('</p></td></tr></table>')[0])
-						except:
-							total_potential += 0
-							total_target += 0			
-						each_customer['potential'] = total_potential
-						each_customer['target'] = total_target
-				else:
-					each_customer['sales_person']=va
-		lst.append(each_customer)
+	if customer_ig:
+		lst.append({"customer":'',"sales_person":''})
+		for key,value in customer_ig.items():
+			total_potential,total_target = 0,0
+			each_customer={}
+			each_customer["customer"]= key
+			# each_customer['sales_person']=
+			for each in value:
+				for ka,va in each.items():
+					if ka !='sales_person':
+						lst[0].update({ka:'	<div style="display: flex;"><p style="margin: 0;text-align:left !important;padding: 0; width: 90px;">Potential</p>|<p style="margin: 0;text-align:right !important;padding: 0; width: 90px;">Target</p></div>'})
+						each_customer[ka]=va
+						if (va):
+							try:
+								total_potential_value= (va.split('padding: 0; width: 90px;">'))
+								total_potential += flt(total_potential_value[1].split('</p>')[0])
+								total_target += flt(total_potential_value[2].split('</p>')[0])
+							except:
+								total_potential += 0
+								total_target += 0			
+							each_customer['potential'] = total_potential
+							each_customer['target'] = total_target
+					else:
+						each_customer['sales_person']=va
+			lst.append(each_customer)
 	return lst
 	
 
