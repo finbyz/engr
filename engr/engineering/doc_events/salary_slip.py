5636bc4073cb777bdbe36bbdee13711489eaf3a7
Ubuntu <ubuntu@ip-172-31-24-118.ap-south-1.compute.internal>
2021-09-15 11:48:17 +0000

    added for date period in Salary Slip

--- engr/engineering/doc_events/salary_slip.py
+++ engr/engineering/doc_events/salary_slip.py
@@ -5,10 +5,8 @@ def validate(self,method):
 
 def get_salary_slip_period(self):
     if self.start_date:
-        frappe.msgprint(int(self.start_date.split('-')[2]), int(self.start_date.split('-')[0]), int(self.start_date.split('-')[1]))
-        start_month = datetime.datetime(int(self.start_date.split('-')[2]), int(self.start_date.split('-')[0]), int(self.start_date.split('-')[1])).strftime("%b")
+        start_month = datetime.datetime(int(self.start_date.split('-')[0]), int(self.start_date.split('-')[1]), int(self.start_date.split('-')[2])).strftime("%B")
         if self.end_date:
-            end_month =datetime.datetime(int(self.end_date.split('-')[2]), int(self.end_date.split('-')[0]), int(self.end_date.split('-')[1])).strftime("%b")
+            end_month =datetime.datetime(int(self.end_date.split('-')[0]), int(self.end_date.split('-')[1]), int(self.end_date.split('-')[2])).strftime("%B")
             if start_month == end_month:
                 self.salary_slip_period = start_month
-
