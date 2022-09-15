dd1de5eae08447e22613d9ee27fb411a90267e25
finbyz <info@finbyz.com>
2022-09-15 09:57:37 +0530

    last

--- engr/engineering/doc_events/proforma_invoice.py
+++ engr/engineering/doc_events/proforma_invoice.py
@@ -2,31 +2,7 @@ import frappe
 from frappe.utils import add_days, cint, cstr, flt, get_link_to_form, getdate, nowdate, strip_html
 from frappe import _
 
-def set_taxes_on_payment_percentage(self,method):
-    doc = frappe.get_doc('GST Settings')
-    account_list = []
-    for row in doc.get('gst_accounts'):
-        account_list.append(row.get('cgst_account'))
-        account_list.append(row.get('sgst_account'))
-        account_list.append(row.get('igst_account'))
-    self.sgst = None
-    self.cgst = None
-    self.igst = None
-   
-    for row in self.taxes:
-        if row.account_head in account_list:
-            if "SGST" in str(row.account_head) or "sgst" in str(row.account_head):
-                payment_percent = (self.total * self.payment_percentage)/100
-                sgst_percent = (payment_percent * row.rate)/100
-                self.sgst = sgst_percent 
-            if "CGST" in str(row.account_head) or "cgst" in str(row.account_head):
-                payment_percent = (self.total * self.payment_percentage)/100
-                cgst_percent = (payment_percent * row.rate)/100
-                self.cgst = cgst_percent
-            if "IGST" in str(row.account_head) or "igst" in str(row.account_head):
-                payment_percent = (self.total * self.payment_percentage)/100
-                igst_percent = (payment_percent * row.rate)/100
-                self.igst = igst_percent
+
 
 def on_cancel(self,method):
     check_nextdoc_docstatus(self,method)
