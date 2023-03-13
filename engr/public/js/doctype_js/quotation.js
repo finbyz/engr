2b2e38774d706bdb6c95e2a8bda9713b92626408
Finbyz <info@finbyz.tech>
2023-03-13 15:26:33 +0530

    Filter changes in Quotation's Sales Order field

--- engr/public/js/doctype_js/quotation.js
+++ engr/public/js/doctype_js/quotation.js
@@ -1,14 +1,14 @@
 frappe.ui.form.on('Quotation', {
-    refresh: function(frm) {
-        
-        frm.add_custom_button(__('Work Order Master'), function(){
-        frappe.model.open_mapped_doc({
-            method: 'engr.engineering.doc_events.quotation.make_WOM',
-            frm: frm,
-        });
-        }, __("Create"));
-        
-    },
+	refresh: function(frm) {
+		
+		frm.add_custom_button(__('Work Order Master'), function(){
+		frappe.model.open_mapped_doc({
+			method: 'engr.engineering.doc_events.quotation.make_WOM',
+			frm: frm,
+		});
+		}, __("Create"));
+		
+	},
 });
 
 cur_frm.fields_dict.project.get_query = function(doc) {
@@ -19,19 +19,19 @@ cur_frm.fields_dict.project.get_query = function(doc) {
 	}
 };
 frappe.ui.form.on("Quotation Item", {
-    item_code:function(frm,cdt,cdn){
-        let d  = locals[cdt][cdn]
-        frappe.model.get_value("Item",d.item_code,"stock_uom"),(r)=>{
-            d.uom = r.stock_uom
-        }
-        
-    } 
+	item_code:function(frm,cdt,cdn){
+		let d  = locals[cdt][cdn]
+		frappe.model.get_value("Item",d.item_code,"stock_uom"),(r)=>{
+			d.uom = r.stock_uom
+		}
+		
+	} 
 });
 cur_frm.fields_dict.sales_order.get_query = function(doc) {
 	return {
 		filters: {
-			"customer": doc.party_name,
-            "docstatus":1
+			"customer": doc.customer_name || doc.party_name,
+			"docstatus":1
 		}
 	}
 };
