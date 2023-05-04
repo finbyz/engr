c077075610a5e48ddac7c214c9d01e7647e963b8
Finbyz <info@finbyz.tech>
2023-05-04 18:19:45 +0530

    changes

--- engr/patch.py
+++ engr/patch.py
@@ -3,9 +3,8 @@ def update_job_id():
     doc_list = frappe.db.get_list("Work Order Master" , pluck="name")
     for row in doc_list:
         doc = frappe.get_doc("Work Order Master" , row)
-        if doc.branch_name:
-            job_id = str(doc.job_id)+"-"+str(doc.branch_name)
-            print(job_id)
-            frappe.db.set_value("Work Order Master" , row , 'job_id' , job_id , update_modified = False)
-
-    frappe.db.sql(""" Update `tabWork Order Master Item` as woi left join `tabWork Order Master` as wom ON wom.name = woi.parent set woi.job_id = wom.job_id """)
+        if doc.proforma_invoice:
+            pro_doc = frappe.db.get_value("Proforma Invoice" , doc.proforma_invoice , 'customer')
+            if doc.customer_name == pro_doc:
+                frappe.db.set_value("Proforma Invoice" , doc.proforma_invoice , 'wom_ref' , row , update_modified =False)
+                frappe.db.set_value("Proforma Invoice" , doc.proforma_invoice , 'work_order_master_ref' , row , update_modified =False)
