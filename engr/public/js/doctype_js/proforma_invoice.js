6a9d7ae792f04c68c9523bb53b304cbbabc1d47a
Finbyz <info@finbyz.tech>
2023-07-10 13:11:44 +0530

    ch

--- engr/public/js/doctype_js/proforma_invoice.js
+++ engr/public/js/doctype_js/proforma_invoice.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Proforma Invoice",{
     onload: function(frm){
-        frm.ignore_doctypes_on_cancel_all = ['Sales Invoice'];
+        frm.ignore_doctypes_on_cancel_all = ['Sales Invoice' , "Sales Invoice Item"];
     
     }
 })
