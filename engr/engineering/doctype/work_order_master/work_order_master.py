fc375314b1f0575ca468a2d22c822b9a9ec0eb73
FinByz Tech Pvt. Ltd. <info@finbyz.tech>
2024-10-29 17:58:59 +0530

    Update work_order_master.py

--- engr/engineering/doctype/work_order_master/work_order_master.py
+++ engr/engineering/doctype/work_order_master/work_order_master.py
@@ -25,6 +25,8 @@ from frappe.utils import (
 
 class WorkOrderMaster(Document):
 	def before_naming(self):
+		if not self.__islocal:
+			return
 		date = frappe.format(self.date , {'fieldtype':'Date'})
 		date = date.split('-')
 
